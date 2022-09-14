a7d34d8ee334d0f298573b29e8ff5a0bf2e43dc8
Parth097 <parthsh197@gmail.com>
2022-09-14 14:31:10 +0100

    migrated code

--- src/data/BYPData/BYPData.ts
+++ src/data/BYPData/BYPData.ts
@@ -8,14 +8,4 @@ const TableHeaderImagesLinks:string[] = [
   "Games/FoodCategories/SweetsWithText.svg",
 ];
 
-export const PlateItemPositions:string[] = [
-  "top-[165px] left-[228px]",
-  "top-[211px] left-[282px]",
-  "top-[280px] left-[264px]",
-  "top-[280px] left-[193px]",
-  "top-[211px] left-[175px]",
-];
-
-export const imageSize = "w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] lg:w-[70px] lg:h-[70px]";
-
 export default TableHeaderImagesLinks;
