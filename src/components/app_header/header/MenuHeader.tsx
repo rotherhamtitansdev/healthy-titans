fe89d8463d7d72653a59db3e451d2631e076bd79
Parth097 <parthsh197@gmail.com>
2022-09-14 13:53:18 +0100

    Changes from old repo to new project

--- src/components/app_header/header/MenuHeader.tsx
+++ src/components/app_header/header/MenuHeader.tsx
@@ -3,11 +3,11 @@ import { HeaderProps } from "../../../models/HeaderProps";
 
 const MenuHeader = (props: HeaderProps) => (
   <div className="flex flex-wrap h-[22rem]">
-    <div className="mx-auto pt-[3rem] md:pt-[4rem]">
+    <div className="mx-auto pt-[3rem] md:pt-[2rem]">
       <h1 className="text-titansDarkBlue font-quicksand font-bold text-center sm:mt-[1rem] text-3xl sm:text-5xl lg:text-6xl mt-8 pb-4">
         {props.title}
       </h1>
-      <div className="font-quicksand font-medium text-homepageHeaderSubTitle text-sm md:text-base lg:text-xl text-center w-2/3 lg:w-[40rem] mx-auto">
+      <div className="font-quicksand font-medium text-homepageHeaderSubTitle text-sm md:text-base lg:text-xl text-center w-2/3 lg:w-[45rem] mx-auto">
         {props.body}
       </div>
     </div>
