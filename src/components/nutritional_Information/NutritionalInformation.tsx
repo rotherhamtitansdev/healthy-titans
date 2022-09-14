2f87d66b320811428c00e2b1e5b048d9efc436b4
Parth097 <parthsh197@gmail.com>
2022-09-14 13:53:18 +0100

    Changes from old repo to new project

--- src/components/nutritional_Information/NutritionalInformation.tsx
+++ src/components/nutritional_Information/NutritionalInformation.tsx
@@ -1,30 +1,12 @@
 import React from "react";
-import { MenuCardProps } from "../../models/MenuCardProps";
-import food from "../../img/foodNutritionPage.jpg";
-import games from "../../img/nutritionNutritionPage.jpg";
-import Menu from "../shared/Menu";
+import NutritionCategory from "./nutritional/NutritionCategory";
+import FoodCategory from "./food/FoodMenu";
 
-const NutritionalInformation = () => {
-  const nutritionalInformationData: MenuCardProps[] = [
-    {
-      key: 0,
-      name: "Food",
-      description: "Information on the food we eat!",
-      img: food,
-      path: "FoodCategory",
-    },
-    {
-      key: 1,
-      name: "Nutrition",
-      description: "Information on what is in our food!",
-      img: games,
-      path: "NutritionCategory",
-    },
-  ];
-
-  return (
-    <Menu header={{ title: "", body: "" }} cards={nutritionalInformationData} />
-  );
-};
+const NutritionalInformation = () => (
+  <>
+    <FoodCategory />
+    <NutritionCategory />
+  </>
+);
 
 export default NutritionalInformation;
