ac39a907f2866489d28a01a80a0864ee069b0ba9
Callum Dixon <callum.dixon@and.digital>
2022-09-14 10:50:43 +0100

    FYP select food items complete, with modal popup helper

--- src/components/games/BuildYourPlate/BuildYourPlateContext.tsx
+++ src/components/games/BuildYourPlate/BuildYourPlateContext.tsx
@@ -8,12 +8,16 @@ export type IsFYPStartedContext = {
   setIsFYPStarted:(c: boolean) => void
   getScore: number
   setScore: (c: number) => void
+  getModal: boolean
+  setModal: (c: boolean) => void
 }
 export const FYPStartedContext = createContext<IsFYPStartedContext>({
   getIsFYPStarted: false,
   setIsFYPStarted: () => {},
   getScore: 0,
   setScore: () => {},
+  getModal: true,
+  setModal: () => {}
 });
 
 export const useFYPStartedContext = () => useContext(FYPStartedContext);
@@ -21,9 +25,10 @@ export const useFYPStartedContext = () => useContext(FYPStartedContext);
 const FYPStartedContextWrapper  = ({ children }:any) => {
   const [getIsFYPStarted, setIsFYPStarted] = useState<boolean>(false);
   const [getScore, setScore] = useState<number>(0)
+  const [getModal, setModal] = useState<boolean>(true)
 
   return (
-    <FYPStartedContext.Provider value={{ getIsFYPStarted, setIsFYPStarted, getScore, setScore }}>
+    <FYPStartedContext.Provider value={{ getIsFYPStarted, setIsFYPStarted, getScore, setScore, getModal, setModal }}>
       <BuildYourPlate/>
     </FYPStartedContext.Provider>
   );
