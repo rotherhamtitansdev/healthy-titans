753cb11e25bcb422dcd74f50af844db1039d38aa
Leah Shields <leah.shields@and.digital>
2022-09-21 14:16:38 +0100

    Added breadcrumbs for food menus

---
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import useBreadcrumbs from "use-react-router-breadcrumbs";
+
+const Breadcrumbs = () => {
+  const routes = [
+    { path: "/", breadcrumb: null },
+    { path: "/NutritionalInformation", breadcrumb: "Food & Nutrition" },
+    { path: "NutritionalInformation/FruitsAndVegetables", breadcrumb: "Fruit & Vegetables" },
+  ];
+
+  const breadcrumbs = useBreadcrumbs(routes);
+
+  return (
+    <div className="font-medium font-quicksand text-[#243A73] md:text-xl lg:text-xl md:pb-5 flex">
+      {breadcrumbs.map(({
+        match,
+        breadcrumb,
+      }, index) => (
+        <>
+          {index !== 0 ? <p className="text text-titansBrightPink">{" > "}</p> : null}
+          <NavLink to={match.pathname}>{breadcrumb}</NavLink>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Breadcrumbs;
