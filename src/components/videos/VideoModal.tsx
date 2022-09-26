5a370018969ede64f7be5a2bc7a7dd2d8dd9f51f
Parth097 <parthsh197@gmail.com>
2022-09-26 11:09:42 +0100

    Rounded off edges, and changed the button styling

--- src/components/videos/VideoModal.tsx
+++ src/components/videos/VideoModal.tsx
@@ -2,19 +2,15 @@ import React from "react";
 import { VideoModalProps } from "../../models/Video";
 import VideoPlayer from "./VideoPlayer";
 
-const VideoModal = (props:VideoModalProps) => (
+const VideoModal = (props: VideoModalProps) => (
   <div>
     {props.getHidden ? null : (
       <>
-        <div
-          className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-        >
+        <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
           <div className="relative w-full sm:w-11/12 md:w-10/12 lg:w-8/12 xl:w-6/12 mx-3 sm:mx-0">
-
-            <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white outline-none focus:outline-none">
-
-              <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                <h3 className="text-3xl font-semibold">
+            <div className="border-0 rounded-lg shadow-lg relative flex flex-col bg-white outline-none focus:outline-none p-[1.3rem]">
+              <div className="flex items-start justify-between pb-3 border-b border-solid border-slate-200 rounded-t">
+                <h3 className="text-2xl font-semibold font-quicksand text-homepageHeaderSubTitle">
                   {props.video.user?.name}
                 </h3>
                 <button
@@ -32,7 +28,7 @@ const VideoModal = (props:VideoModalProps) => (
 
               <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b mx-auto">
                 <button
-                  className="bg-blue-600 text-white font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  className="font-quicksand font-bold bg-titansBrightPink text-white font-bold font-quicksand py-2 px-8 rounded-full"
                   type="button"
                   onClick={() => props.setHidden(true)}
                 >
