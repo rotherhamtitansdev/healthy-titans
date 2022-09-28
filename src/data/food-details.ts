fd59977decfb7164069dc0d0473814e1f77fd3ce
Tom Downes <tom.downes@and.digital>
2022-09-28 11:39:12 +0100

    update fast food, sweets, dairy & eggs

--- src/data/food-details.ts
+++ src/data/food-details.ts
@@ -187,146 +187,222 @@ const FoodDetailsComponentData = {
   Butter: {
     name: "Butter",
     description:
-      "Butter Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Butter is a healthier alternative to margarine spreads as they contain TRANS FATS. Trans fats have been proven to increase the risk of heart disease, stroke, diabetes and mortality!",
     firebaseName: "FoodCategories/DairyAndEggs/Food/Butter.svg",
     category: "Dairy & Eggs",
+    facts: [
+      "Butter from grass fed sources contain the essential omega -3 fatty acids.",
+      "Source of VITAMIN B12 which contributes to healthy nerve cells, production of DNA and red blood cell production.",
+      "Source of the FAT - soluble vitamins A, D & E.",
+      "VITAMIN A which has multiple benefits to children including helping with the maintenance of healthy bones, immune system and soft tissues such as muscle. Vitamin A also plays a role in having healthy skin and sight.",
+      "Source of VITAMIN D which has a range of benefits in children including improved bone health via vitamin D supporting calcium absorption, improves immune system and has been shown to reduce depression symptoms.",
+      "VITAMIN E acts as an antioxidant to prevent cell damage.",
+    ],
     score: 7,
   },
   Cheese: {
     name: "Cheese",
     description:
-      "Cheese Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+      "Where can we include cheese in our diets? Cheese is great to have in your sandwiches, on pasta bakes, jacket potato or with crackers!",
     firebaseName: "FoodCategories/DairyAndEggs/Food/Cheese.svg",
     category: "Dairy & Eggs",
+    facts: [
+      "Rich source of CALCIUM which helps build bones and teeth.",
+      "Source of PHOSPHORUS which helps with bone formation and prevents the loss of calcium from bone.",
+      "Source of FAT and high source of PROTEIN.",
+      "Rich source of VITAMIN B12 which contributes to healthy nerve cells, production of DNA and red blood cell production.",
+      "Full fat varieties have VITAMIN A.",
+      "VITAMIN A which has multiple benefits to children including helping with the maintenance of healthy bones, immune system and soft tissues such as muscle. Vitamin A also plays a role in having healthy skin and sight.",
+    ],
     score: 8,
   },
   Eggs: {
     name: "Eggs",
     description:
-      "Eggs Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Eggs can be boiled, fried, scrambled, poached and baked. Boiled eggs have the highest protein value.",
     firebaseName: "FoodCategories/DairyAndEggs/Food/Eggs.svg",
     category: "Dairy & Eggs",
+    facts: [
+      "Source of Biotin which is beneficial for hair and nail health.",
+      "Egg yolks are a good source of VITAMIN K which is important in blood clotting and bone formation.",
+      "Good source of PROTEIN.",
+      "Moderate amount of VITAMIN B2 which works as an ANTIOXIDANT.",
+      "Good source of FAT which comes from the yolk.",
+      "Rich source of CHOLINE which is necessary for brain development.",
+      "Rich source of vitamin B12 which contributes to healthy nerve cells, production of DNA and red blood cell production.",
+      "Vitamin B12 deficiency symptoms include fatigue, feeling of faint, breathlessness and headaches.",
+      "Source of VITAMIN D which has a range of benefits in children including improved bone health via vitamin D supporting calcium absorption, improves immune system and has been shown to reduce depression symptoms.",
+    ],
     score: 9,
   },
   Milk: {
     name: "Milk",
-    description:
-      "Milk Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    description: "Did you know CALCIUM deficiency can cause RICKETS in children?",
     firebaseName: "FoodCategories/DairyAndEggs/Food/Milk.svg",
     category: "Dairy & Eggs",
+    facts: [
+      "Whole milk contains VITAMIN A which has multiple benefits to children including helping with the maintenance of healthy bones, immune system and soft tissues such as muscle. Vitamin A also plays a role in having healthy skin and sight.",
+      "Source of VITAMIN D which has a range of benefits in children including improved bone health via vitamin D supporting calcium absorption, improves immune system and has been shown to reduce depression symptoms.",
+      "Rich source of CALCIUM which helps build bones and teeth.",
+      "Good source of FAT and PROTEIN.",
+    ],
     score: 9,
   },
   Yogurt: {
     name: "Yogurt",
-    description:
-      "Yogurt Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "Did you know CALCIUM deficiency can cause RICKETS in children?",
     firebaseName: "FoodCategories/DairyAndEggs/Food/Yoghurt.svg",
     category: "Dairy & Eggs",
+    facts: [
+      "Yoghurt is a good source of PHOSPHORUS, an essential mineral that plays an important role in biological processes.",
+      "Good source of PROTEIN.",
+      "Rich source of CALCIUM which helps build bones and teeth.",
+      "Good source of the B-complex vitamins which help the body convert food into fuel.",
+    ],
     score: 9,
   },
   // Junk Food
   Burger: {
     name: "Hamburger",
-    description:
-      "Hamburger Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/JunkFood/Food/Burger.svg",
     category: "Fast Food",
+    facts: [
+      "Think about if the burger meat is processed and what the burger also consists of. Can we make this healthier?",
+      "Consider making homemade burgers using unprocessed meat, adding vegetables of your choice and making your own sauces. This is very fun too!",
+    ],
     score: 3,
   },
   Chips: {
     name: "French Fries",
-    description:
-      "French Fries Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/JunkFood/Food/Chips.svg",
     category: "Fast Food",
+    facts: [
+      "Try pre-cut vegetable sticks with a low-calorie dip like hummus or greek yoghurt. Banana bread, rice cakes, walnuts or beef jerky are also good snack options.",
+    ],
     score: 3,
   },
   FizzyDrinks: {
     name: "Coke",
-    description:
-      "Coke Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/JunkFood/Food/FizzyDrinks.svg",
     category: "Fast Food",
+    facts: [
+      'If you regularly consume fizzy drinks then try look for sugar free alternatives initially. They usually contain the words "zero" "free" or "diet" on them.',
+    ],
     score: 2,
   },
   FriedChicken: {
     name: "Fried Chicken",
-    description:
-      "Fried Chicken Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/JunkFood/Food/FriedChicken.svg",
     category: "Fast Food",
+    facts: [
+      "Deep-fried chicken typically high in trans fats which have been proven to increase the risk of heart disease, stroke, diabetes and mortality!",
+      "Why not bake or grill the chicken instead? you still achieve similar flavour of chicken and the same texture of the meat too. Seasoning chicken with a pinch of salt, pepper, rosemary, paprika or garlic powder can add some nice flavour.",
+    ],
     score: 2,
   },
   Hotdogs: {
     name: "Hotdogs",
-    description:
-      "Hotdogs Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/JunkFood/Food/HotDog.svg",
     category: "Fast Food",
+    facts: [
+      "Think about if the sausage meat is processed and what the hot dog consists of. Can we make this healthier?",
+      "Consider making your own homemade hotdogs by using unprocessed meat, adding vegetables of your choice and making your own sauces. This is very fun too!",
+    ],
     score: 2,
   },
   Milkshakes: {
     name: "Chocolate Milkshakes",
-    description:
-      "Chocolate Milkshakes Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/DairyAndEggs/Food/Milkshakes.svg",
     category: "Fast Food",
+    facts: [
+      "Whole milk contains VITAMIN A which has multiple benefits to children including helping with the maintenance of healthy bones, immune system and soft tissues such as muscle. Vitamin A also plays a role in having healthy skin and sight.",
+      "Source of vitamin D which has a range of benefits in children including improved bone health via vitamin D supporting calcium absorption, improves immune system and has been shown to reduce depression symptoms.",
+      "Rich source of CALCIUM which helps build bones and teeth.",
+      "Good source of PROTEIN",
+    ],
     score: 2,
   },
   Pizza: {
     name: "Pizza",
-    description:
-      "Pizza Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/JunkFood/Food/Pizza.svg",
     category: "Fast Food",
+    facts: [
+      "Many frozen pizzas contain trans fats which have been proven to increase the risk of heart disease, stroke, diabetes and mortality!",
+      "Try making a healthy homemade pizza! Making your own dough and adding vegetables, meat and even some fruits like pineapple can be very fun!",
+    ],
     score: 2,
   },
   Chocolate: {
     name: "Chocolate",
-    description:
-      "Chocolate Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/Sweets/Food/Chocolate.svg",
     category: "Sweets",
+    facts: [
+      "If we don’t buy snacks like chocolate, sweets or cake to have available at home then we’re less likely to consume them. Buy fresh fruit to have available at home as a healthier and more nutritious snack option! Have you tried dark chocolate instead or normal chocolate?",
+      "Try pre-cut vegetable sticks with a low-calorie dip like hummus or greek yoghurt. Banana bread, rice cakes, walnuts or beef jerky are also good snack options.",
+    ],
     score: 2,
   },
   JellySweets: {
     name: "Jelly Sweets",
-    description:
-      "Jelly Sweets Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/Sweets/Food/JellySweets.svg",
     category: "Sweets",
+    facts: [
+      "If we don’t buy snacks like chocolate, sweets or cake to have available at home then we’re less likely to consume them. Buy fresh fruit to have available at home as a healthier and more nutritious snack option! Have you tried dark chocolate instead or normal chocolate?",
+      "Try pre-cut vegetable sticks with a low-calorie dip like hummus or greek yoghurt. Banana bread, rice cakes, walnuts or beef jerky are also good snack options.",
+    ],
     score: 2,
   },
   Cake: {
     name: "Cupcakes",
-    description:
-      "Cupcakes Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/Sweets/Food/Cake.svg",
     category: "Sweets",
+    facts: [
+      "If we don’t buy snacks like chocolate, sweets or cake to have available at home then we’re less likely to consume them. Buy fresh fruit to have available at home as a healthier and more nutritious snack option! Have you tried dark chocolate instead or normal chocolate?",
+      "Try pre-cut vegetable sticks with a low-calorie dip like hummus or greek yoghurt. Banana bread, rice cakes, walnuts or beef jerky are also good snack options.",
+    ],
     score: 2,
   },
   Biscuits: {
     name: "Biscuits",
-    description:
-      "Biscuits Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/Sweets/Food/Biscuit.svg",
     category: "Sweets",
+    facts: [
+      "If we don’t buy snacks like chocolate, sweets or cake to have available at home then we’re less likely to consume them. Buy fresh fruit to have available at home as a healthier and more nutritious snack option! Have you tried dark chocolate instead or normal chocolate?",
+      "Try pre-cut vegetable sticks with a low-calorie dip like hummus or greek yoghurt. Banana bread, rice cakes, walnuts or beef jerky are also good snack options.",
+    ],
     score: 2,
   },
   Donuts: {
     name: "Donuts",
-    description:
-      "Donuts Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/Sweets/Food/Donut.svg",
     category: "Sweets",
+    facts: [
+      "If we don’t buy snacks like chocolate, sweets or cake to have available at home then we’re less likely to consume them. Buy fresh fruit to have available at home as a healthier and more nutritious snack option! Have you tried dark chocolate instead or normal chocolate?",
+      "Try pre-cut vegetable sticks with a low-calorie dip like hummus or greek yoghurt. Banana bread, rice cakes, walnuts or beef jerky are also good snack options.",
+    ],
     score: 2,
   },
   IceCream: {
     name: "Ice Cream",
-    description:
-      "Ice Cream Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    description: "It's okay to have these in moderation - little and not frequent.",
     firebaseName: "FoodCategories/Sweets/Food/IceCream.svg",
     category: "Sweets",
+    facts: [
+      "If we don’t buy snacks like chocolate, sweets or cake to have available at home then we’re less likely to consume them. Buy fresh fruit to have available at home as a healthier and more nutritious snack option! Have you tried dark chocolate instead or normal chocolate?",
+      "Try pre-cut vegetable sticks with a low-calorie dip like hummus or greek yoghurt. Banana bread, rice cakes, walnuts or beef jerky are also good snack options.",
+    ],
     score: 2,
   },
 };
