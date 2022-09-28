1ca8075e7263c74df098093a6c538aa773835e8a
Tom Downes <tom.downes@and.digital>
2022-09-28 10:38:19 +0100

    temporary food-details data file

---
+++ src/data/food-details.ts
@@ -0,0 +1,334 @@
+// eslint-disable-next-line unused-imports/no-unused-vars, @typescript-eslint/no-unused-vars
+const FoodDetailsComponentData = {
+  Beef: {
+    name: "Beef",
+    description:
+      "Beef is a very good source of protein and can be bought and cooked in many different ways...",
+    firebaseName: "FoodCategories/Meat/Food/Beef.svg",
+    category: "Meat",
+    score: 9,
+  },
+  Chicken: {
+    name: "Chicken",
+    description:
+      "Chicken Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Meat/Food/Chicken.svg",
+    category: "Meat",
+    score: 9,
+  },
+  Pork: {
+    name: "Pork",
+    description:
+      "Pork Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Meat/Food/Pork.svg",
+    category: "Meat",
+    score: 8,
+  },
+  Lamb: {
+    name: "Lamb",
+    description:
+      "Lamb Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Meat/Food/Lamb.svg",
+    category: "Meat",
+    score: 9,
+  },
+  CookedMeats: {
+    name: "Cooked Meats",
+    description:
+      "Cooked Meats Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Meat/Food/CookedMeats.svg",
+    category: "Meat",
+    score: 8,
+  },
+  Cod: {
+    name: "Cod",
+    description:
+      "Cod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fish/Food/Cod.svg",
+    category: "Fish",
+    score: 7,
+  },
+  Salmon: {
+    name: "Salmon",
+    description:
+      "Cod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fish/Food/Salmon.svg",
+    category: "Fish",
+    score: 10,
+  },
+  Tuna: {
+    name: "Tuna",
+    description:
+      "Cod Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fish/Food/Tuna.svg",
+    category: "Fish",
+    score: 9,
+  },
+  Apples: {
+    name: "Apples",
+    description:
+      "Apples Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fruits/Food/Apple.svg",
+    category: "Fruit",
+    score: 7,
+  },
+  Bananas: {
+    name: "Bananas",
+    description:
+      "Bananas Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fruits/Food/Banana.svg",
+    category: "Fruit",
+    score: 9,
+  },
+  Grapes: {
+    name: "Grapes",
+    description:
+      "Grapes Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fruits/Food/Grapes.svg",
+    category: "Fruit",
+    score: 7,
+  },
+  Oranges: {
+    name: "Oranges",
+    description:
+      "Oranges Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fruits/Food/Orange.svg",
+    category: "Fruit",
+    score: 7,
+  },
+  Pears: {
+    name: "Pears",
+    description:
+      "Pears Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fruits/Food/Pear.svg",
+    category: "Fruit",
+    score: 7,
+  },
+  Strawberries: {
+    name: "Strawberries",
+    description:
+      "Strawberries Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fruits/Food/Strawberry.svg",
+    category: "Fruit",
+    score: 7,
+  },
+  BellPeppers: {
+    name: "Bell Peppers",
+    description:
+      "Bell Peppers Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Vegetables/Food/BellPepper.svg",
+    category: "Veg",
+    score: 9,
+  },
+  Broccoli: {
+    name: "Broccoli",
+    description:
+      "Broccoli Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Vegetables/Food/Broccoli.svg",
+    category: "Veg",
+    score: 9,
+  },
+  Carrot: {
+    name: "Carrots",
+    description:
+      "Carrots Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Vegetables/Food/Carrot.svg",
+    category: "Veg",
+    score: 7,
+  },
+  Cauliflower: {
+    name: "Cauliflower",
+    description:
+      "Cauliflower Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Vegetables/Food/Cauliflower.svg",
+    category: "Veg",
+    score: 9,
+  },
+  Mushrooms: {
+    name: "Mushrooms",
+    description:
+      "Mushrooms Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Vegetables/Food/Mushroom.svg",
+    category: "Veg",
+    score: 7,
+  },
+  Peas: {
+    name: "Peas",
+    description:
+      "Peas Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Vegetables/Food/Peas.svg",
+    category: "Veg",
+    score: 8,
+  },
+  Potato: {
+    name: "Potato",
+    description:
+      "Potato Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Vegetables/Food/Potato.svg",
+    category: "Veg",
+    score: 7,
+  },
+  Tomato: {
+    name: "Tomato",
+    description:
+      "Tomato Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Fruits/Food/Tomato.svg",
+    category: "Veg",
+    score: 8,
+  },
+  Spinach: {
+    name: "Spinach",
+    description:
+      "Spinach Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/Vegetables/Food/Spinach.svg",
+    category: "Veg",
+    score: 8,
+  },
+  Butter: {
+    name: "Butter",
+    description:
+      "Butter Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/DairyAndEggs/Food/Butter.svg",
+    category: "Dairy & Eggs",
+    score: 7,
+  },
+  Cheese: {
+    name: "Cheese",
+    description:
+      "Cheese Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/DairyAndEggs/Food/Cheese.svg",
+    category: "Dairy & Eggs",
+    score: 8,
+  },
+  Eggs: {
+    name: "Eggs",
+    description:
+      "Eggs Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/DairyAndEggs/Food/Eggs.svg",
+    category: "Dairy & Eggs",
+    score: 9,
+  },
+  Milk: {
+    name: "Milk",
+    description:
+      "Milk Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    firebaseName: "FoodCategories/DairyAndEggs/Food/Milk.svg",
+    category: "Dairy & Eggs",
+    score: 9,
+  },
+  Yogurt: {
+    name: "Yogurt",
+    description:
+      "Yogurt Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/DairyAndEggs/Food/Yoghurt.svg",
+    category: "Dairy & Eggs",
+    score: 9,
+  },
+  // Junk Food
+  Burger: {
+    name: "Hamburger",
+    description:
+      "Hamburger Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/JunkFood/Food/Burger.svg",
+    category: "Fast Food",
+    score: 3,
+  },
+  Chips: {
+    name: "French Fries",
+    description:
+      "French Fries Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/JunkFood/Food/Chips.svg",
+    category: "Fast Food",
+    score: 3,
+  },
+  FizzyDrinks: {
+    name: "Coke",
+    description:
+      "Coke Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/JunkFood/Food/FizzyDrinks.svg",
+    category: "Fast Food",
+    score: 2,
+  },
+  FriedChicken: {
+    name: "Fried Chicken",
+    description:
+      "Fried Chicken Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/JunkFood/Food/FriedChicken.svg",
+    category: "Fast Food",
+    score: 2,
+  },
+  Hotdogs: {
+    name: "Hotdogs",
+    description:
+      "Hotdogs Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/JunkFood/Food/HotDog.svg",
+    category: "Fast Food",
+    score: 2,
+  },
+  Milkshakes: {
+    name: "Chocolate Milkshakes",
+    description:
+      "Chocolate Milkshakes Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/DairyAndEggs/Food/Milkshakes.svg",
+    category: "Fast Food",
+    score: 2,
+  },
+  Pizza: {
+    name: "Pizza",
+    description:
+      "Pizza Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/JunkFood/Food/Pizza.svg",
+    category: "Fast Food",
+    score: 2,
+  },
+  Chocolate: {
+    name: "Chocolate",
+    description:
+      "Chocolate Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Sweets/Food/Chocolate.svg",
+    category: "Sweets",
+    score: 2,
+  },
+  JellySweets: {
+    name: "Jelly Sweets",
+    description:
+      "Jelly Sweets Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Sweets/Food/JellySweets.svg",
+    category: "Sweets",
+    score: 2,
+  },
+  Cake: {
+    name: "Cupcakes",
+    description:
+      "Cupcakes Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Sweets/Food/Cake.svg",
+    category: "Sweets",
+    score: 2,
+  },
+  Biscuits: {
+    name: "Biscuits",
+    description:
+      "Biscuits Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Sweets/Food/Biscuit.svg",
+    category: "Sweets",
+    score: 2,
+  },
+  Donuts: {
+    name: "Donuts",
+    description:
+      "Donuts Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Sweets/Food/Donut.svg",
+    category: "Sweets",
+    score: 2,
+  },
+  IceCream: {
+    name: "Ice Cream",
+    description:
+      "Ice Cream Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et  dolore magna aliqua.",
+    firebaseName: "FoodCategories/Sweets/Food/IceCream.svg",
+    category: "Sweets",
+    score: 2,
+  },
+};
+
+export {};
