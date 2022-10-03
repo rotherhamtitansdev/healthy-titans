d4a83ce93f334c7168bcab8d79a7c670cae8ca6e
Parth097 <parthsh197@gmail.com>
2022-10-03 05:14:36 +0100

    Styling for Details Card/Component

--- src/components/shared/DetailsCard.tsx
+++ src/components/shared/DetailsCard.tsx
@@ -8,14 +8,21 @@ import Image from "./Image";
  * This component represents a details card
  * */
 const DetailsCard = (props: DetailsCardProps) => (
-  <Card card={{ name: props.name, additionalStyling: `rounded-3xl flex-initial ${props.additionalStyling || ""}` }}>
+  <Card
+    card={{
+      name: props.name,
+      additionalStyling: `rounded-3xl flex-initial ${
+        props.additionalStyling || ""
+      }`,
+    }}
+  >
     {props.img && <Image img={props.img} alt={props.name} />}
     <div className="pl-4 pr-4">
       {!props.hideTitle ? (
         <div className="tracking-wide text-lg text-black font-bold">
           {props.name}
         </div>
-      ) : (null)}
+      ) : null}
       <p className="block mt-1 text-lg leading-tight font-semibold text-gray-900">
         {props.description}
       </p>
