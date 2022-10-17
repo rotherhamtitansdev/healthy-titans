d3e91c6ba4f21022d8a26f1b351b051bfca7f9cb
Tom Downes <tom.downes@and.digital>
2022-10-17 15:25:26 +0100

    FoodDetailsComponent test tweaks

--- src/components/nutritional_Information/food_details/FoodDetailsComponent.test.tsx
+++ src/components/nutritional_Information/food_details/FoodDetailsComponent.test.tsx
@@ -3,6 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { MemoryRouter, Route, Routes } from "react-router";
 import FirebaseAPI from "../../../api/FirebaseAPI";
 import FoodDetailsComponent from "./FoodDetailsComponent";
+import { FoodDetailsProps } from "../../../data/nutritional_information/FoodDetailsComponentData";
 
 const mockContent = {
   cardData: [
@@ -36,15 +37,31 @@ const mockContent = {
 
 describe("Food Details Component", () => {
   test("component renders both correct data and image from path and a carousel", async () => {
-    jest.spyOn(FirebaseAPI, "fetchFoodDetailsSeeNext").mockResolvedValue(mockContent);
-    jest.spyOn(FirebaseAPI, "fetchFoodDetailsSingle").mockResolvedValue(mockContent.cardData[2]);
+    const mockFetchSeeNext = jest.fn((category, currentName) => {
+      const filteredCardData: FoodDetailsProps[] = mockContent.cardData.filter(
+        (cardData: FoodDetailsProps) =>
+          cardData.category === category && cardData.name !== currentName
+      );
+      const filteredPaths: string[] = mockContent.paths.filter((path) => path !== currentName);
+      return Promise.resolve({ cardData: filteredCardData, paths: filteredPaths });
+    });
+    const mockFetchFoodSingle = jest.fn().mockResolvedValueOnce(mockContent.cardData[2]);
+    // const mock;
+    jest.spyOn(FirebaseAPI, "fetchFoodDetailsSeeNext").mockImplementationOnce(mockFetchSeeNext);
+    jest.spyOn(FirebaseAPI, "fetchFoodDetailsSingle").mockImplementationOnce(mockFetchFoodSingle);
     jest.spyOn(FirebaseAPI, "fetchImages").mockResolvedValue("mock/Image3.svg");
 
-    // define width for carousel to appear
+    // define width for carousel to appear & prevent buttons from loading
     Object.defineProperties(window.HTMLElement.prototype, {
       offsetWidth: {
         get: () => 1400,
       },
+    });
+
+    Object.defineProperty(window, "innerWidth", {
+      writable: true,
+      configurable: true,
+      value: 1400,
     });
 
     // Using memoryrouter to mock Route as path to the component is required for the contents
@@ -61,16 +78,17 @@ describe("Food Details Component", () => {
         </Routes>
       </MemoryRouter>
     );
-    const mockTextToFind = await screen.findAllByText("mock3");
-    const mockButtonToFind = await screen.findAllByRole("button");
 
-    expect(mockTextToFind[0]).toBeVisible();
-    expect(await screen.findByText("mockFact23")).toBeVisible();
-    expect(await screen.findByRole("img", { name: "mock3" })).toHaveAttribute(
-      "src",
-      "mock/Image3.svg"
-    );
-    expect(mockButtonToFind[0]).toHaveAttribute("data-testid", "mock1");
+    expect(await screen.findByText("mock3")).toBeVisible();
+    expect(screen.getByText("mockFact23")).toBeVisible();
+    expect(screen.getByRole("img", { name: "mock3" })).toHaveAttribute("src", "mock/Image3.svg");
+    expect(screen.getAllByRole("button")).toHaveLength(2);
+    expect(screen.getByRole("button", { name: "mock1" })).toBeVisible();
+    expect(screen.queryByRole("button", { name: "mock3" })).not.toBeInTheDocument();
+
+    expect(mockFetchFoodSingle).toHaveBeenCalledWith("mock3");
+    expect(mockFetchSeeNext).toHaveBeenCalledWith("mockCategory", "mock3");
+
     expect(asFragment()).toMatchSnapshot();
   });
 });
