c7ca193aa556d08ff56fe2f3ac8e898163e73637
ALougherANDDigital <andrew.lougher@and.digital>
2022-10-19 08:56:41 +0100

    Test Component Snapshot test

--- src/components/shared/ImageTest.test.tsx
+++ src/components/shared/ImageTest.test.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Image from "./Image";
 
+const testAlt = "testAlt";
+const testURI = "testURI.svg";
+
 describe("Image Component", () => {
   test("renders image with correct alt and name", () => {
-    const testAlt = "testAlt";
-    const testURI = "testURI.svg";
-    const { asFragment } = render(<Image img={testURI} alt={testAlt} />);
+    const testClassName = "testClassName";
+    const { asFragment } = render(
+      <Image img={testURI} alt={testAlt} additionalStyling={testClassName} />
+    );
+    const testImage = screen.getByRole("img", { name: testAlt });
 
-    expect(screen.getByRole("img", { name: testAlt })).toHaveAttribute("src", testURI);
+    expect(testImage).toHaveAttribute("src", testURI);
+    expect(testImage).toHaveAttribute("class", `mx-auto ${testClassName}`);
     expect(asFragment()).toMatchSnapshot();
   });
 });
