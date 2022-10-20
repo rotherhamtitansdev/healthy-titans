98a071b82a7c43ee4fbdbb0662e78531577de90d
Parth097 <parthsh197@gmail.com>
2022-10-20 14:49:48 +0100

    resolved merge issues

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -23,39 +23,37 @@ const App: React.FC = () => (
   <div className="layout-container">
     <Navbar />
     <main>
-      <Routes>
-        <Route path="/" />
-        <Route index element={<HomePageComponents />} />
-        <Route path="NutritionalInformation">
-          <Route index element={<NutritionalInformation />} />
-          {["Fat", "SaltAndSugar", "Water", "Protein", "Carbs", "MacroAndMicroNutrients"].map(
-            (nutritionCategory) => (
-              <Route
-                key={nutritionCategory}
-                path={nutritionCategory}
-                element={<NutritionDetailsComponent nutritionName={nutritionCategory} />}
-              />
-            )
-          )}
-          <Route path="FruitAndVegetables">
-            <Route index element={<FoodCategory />} />
-            <Route path=":foodCategory">
-              <Route index element={<FoodCategory />} />
-              <Route path=":foodName" element={<FoodDetailsComponent />} />
-            </Route>
-          </Route>
+    <Routes>
+      <Route path="/"/>
+      <Route index element={<HomePageComponents />} />
+      <Route path="NutritionalInformation">
+        <Route index element={<NutritionalInformation showFood/>} />
+
+        <Route path="Nutrition">
+          <Route index element={<NutritionalInformation showFood={false}/>}/>
+          <Route path=":nutritionName" element={<NutritionDetailsComponent />}/>
+        </Route>
+
+        <Route path=":foodCategory">
+          <Route index element={<FoodCategory />} />
+          <Route path=":foodName" element={<FoodDetailsComponent />} />
+        </Route>
+
+        <Route path="FruitAndVegetables">
+          <Route index element={<FoodCategory />} />
           <Route path=":foodCategory">
             <Route index element={<FoodCategory />} />
             <Route path=":foodName" element={<FoodDetailsComponent />} />
           </Route>
         </Route>
-        <Route path="FitnessChallenges">
-          <Route index element={<Challenges />} />
-          <Route path=":challengeName" element={<ChallengeDetailsComponent />} />
-        </Route>
-        <Route path="Videos">
-          <Route index element={<VideosComponent />} />
-        </Route>
+      </Route>
+      <Route path="FitnessChallenges">
+        <Route index element={<Challenges />} />
+        <Route path=":challengeName" element={<ChallengeDetailsComponent />} />
+      </Route>
+      <Route path="Videos">
+        <Route index element={<VideosComponent />} />
+      </Route>
         <Route path="Recipes">
           <Route index element={<Recipes />} />
         </Route>
