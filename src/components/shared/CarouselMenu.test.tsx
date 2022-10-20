f3eb227c6a3e23bae1771b6e016fe93783de5f80
ccarney96 <113630155+ccarney96@users.noreply.github.com>
2022-10-20 10:54:51 +0100

    remove waitFor import

--- src/components/shared/CarouselMenu.test.tsx
+++ src/components/shared/CarouselMenu.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import React from "react";
 import CarouselMenu from "./CarouselMenu";
