80fb8101ee52b7b6057063e37f4f2c22ca6d3e85
AB <ashleybeel@icloud.com>
2022-10-24 11:30:07 +0100

    Only show rules if rules array is not empty

--- src/components/fitness_Challenges/challenge_details/ChallengeDetailsComponent.tsx
+++ src/components/fitness_Challenges/challenge_details/ChallengeDetailsComponent.tsx
@@ -69,18 +69,22 @@ const ChallengeDetailsComponent = () => {
                   ))}
                 </ul>
               </div>
-              <div className="pt-4 pb-3 tracking-wide text-2xl font-quicksand text-homepageHeaderSubTitle font-semibold">
-                Rules
-              </div>
-              <div className="pl-4 block mt-1 text-lg leading-tight font-medium font-quicksand">
-                <ul className="list-disc">
-                  {getChallengeDetailsComponentData.rules.map((rule) => (
-                    <li className="pb-6" key={rule}>
-                      {rule}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              {getChallengeDetailsComponentData.rules.length >= 1 && (
+                <>
+                  <div className="pt-4 pb-3 tracking-wide text-2xl font-quicksand text-homepageHeaderSubTitle font-semibold">
+                    Rules
+                  </div>
+                  <div className="pl-4 block mt-1 text-lg leading-tight font-medium font-quicksand">
+                    <ul className="list-disc">
+                      {getChallengeDetailsComponentData.rules.map((rule) => (
+                        <li className="pb-6" key={rule}>
+                          {rule}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </>
+              )}
             </div>
           </div>
         </DetailsComponent>
