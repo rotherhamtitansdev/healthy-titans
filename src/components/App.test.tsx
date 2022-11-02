9cd4199aea9836e2eb038f1304ec1459c4dde4c9
ALougherANDDigital <andrew.lougher@and.digital>
2022-11-02 15:07:05 +0000

    app test no loose async

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,24 +2,37 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import { MemoryRouter } from "react-router";
+import FirebaseAPI from "../api/FirebaseAPI";
 import { MenuCardProps } from "../models/MenuCardProps";
 import App from "./App";
 
 const mockHomeContent: MenuCardProps[] = [
   {
     key: 0,
+    name: "Food & Nutrition",
+    path: "/FoodAndNutrition",
+    firebaseName: "HomePageImages/apple.svg",
+  },
+  {
+    key: 1,
+    name: "Fitness Challenges",
+    path: "/FitnessChallenges",
+    firebaseName: "HomePageImages/running.svg",
+  },
+  {
+    key: 2,
     name: "Games",
     path: "/Games",
     firebaseName: "HomePageImages/games.svg",
   },
   {
-    key: 1,
+    key: 3,
     name: "Videos",
     path: "/Videos",
     firebaseName: "HomePageImages/film.svg",
   },
   {
-    key: 2,
+    key: 4,
     name: "Recipes",
     path: "/Recipes",
     firebaseName: "HomePageImages/notepad.svg",
@@ -33,11 +46,11 @@ test("home page renders", async () => {
       get: () => 100,
     },
   });
+  jest.spyOn(FirebaseAPI, "fetchDataFromPath").mockResolvedValue(mockHomeContent);
 
   render(<App />, { wrapper: MemoryRouter });
 
   expect(await screen.findByRole("button", { name: "Get started" })).toBeVisible();
-
   mockHomeContent.forEach(async (element) => {
     expect(
       await screen.findByRole("button", { name: element.name, hidden: true })
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -101,5 +101,4 @@ const App: React.FC = () => (
     <Footer />
   </div>
 );
-
 export default App;
