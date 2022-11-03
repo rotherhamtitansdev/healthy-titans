29b6511df7a24ce33f7e685b994d6a6020c046ec
Robert Sayles <robert.sayles@and.digital>
2022-11-03 15:48:20 +0000

    [ROT-377]: Added cy tests for get started button and mobile menu

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -7,5 +7,31 @@ describe("Homepage", () => {
     cy.checkA11y(null, null, terminalLog, true);
   });
 
-  it("Can see call to action and carousel items");
+  it("Can see call to action and carousel items", () => {
+    cy.visit("http://localhost:3000")
+    cy.window().its("scrollY").should("equal", 0);
+    cy.contains("Get started").click()
+    cy.window().its("scrollY").should("equal", 400);
+    cy.get(".react-multi-carousel-list").should("be.visible")
+    cy.get(".react-multi-carousel-item").should("have.length", 5)
+  });
+
+  it("Can toggle mobile menu and see options when menu is open", () => {
+    cy.viewport(768, 1024)
+    cy.visit("http://localhost:3000");
+    cy.get("[aria-label='menu-toggle'").should("be.visible")
+    cy.get('a[href="/FoodAndNutrition"]').should("not.be.visible")
+    cy.contains("Home").should("not.exist")
+    cy.get("[aria-label='menu-toggle'").click()
+    cy.contains("Home")
+    cy.get("[aria-label='menu-toggle'").click()
+    cy.contains("Home").should("not.exist")
+  })
+
+  it("Has top navbar instead of mobile menu when page is wide enough", () => {
+    cy.viewport(1440, 900)
+    cy.visit("http://localhost:3000");
+    cy.get("[aria-label='menu-toggle'").should("not.be.visible")
+    cy.get('a[href="/FoodAndNutrition"]').should("be.visible")
+  })
 });
