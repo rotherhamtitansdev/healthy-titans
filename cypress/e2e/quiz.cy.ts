4e60fdb01522e02f14e29f409fb0d9c9bdc03cd7
AshleyBeel <46923259+AshleyBeel@users.noreply.github.com>
2022-11-07 14:29:26 +0000

    Inital commit

--- cypress/e2e/quiz.cy.ts
+++ cypress/e2e/quiz.cy.ts
@@ -1,4 +1,19 @@
+import terminalLog from "../support/terminal-log";
+
 describe("Quiz", () => {
-  it("gives correct score with 4 correct answers");
-  it("gives correct score with 2 correct answers");
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/Games/Quiz");
+  });
+  
+  it("check a11y Quiz page", () => {
+    cy.injectAxe();
+    cy.checkA11y(null, null, terminalLog, true);
+  });
+ 
+  it("gives correct score with 4 correct answers", () => {
+    cy.contains("BEGIN").click()
+    // cy.wait(5000);
+    cy.contains("Pork", {timeout:10000}).should('be.visible')
+  });
+  // it("gives correct score with 2 correct answers");
 });
