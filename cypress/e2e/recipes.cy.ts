0a05dcd89b6da801b7c90a2be2017c0c868798ae
ccarney96 <113630155+ccarney96@users.noreply.github.com>
2022-11-07 09:16:54 +0000

    recipe's open in new tab test

--- cypress/e2e/recipes.cy.ts
+++ cypress/e2e/recipes.cy.ts
@@ -1,11 +1,20 @@
 import terminalLog from "../support/terminal-log";
 
 describe("Recipes", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/recipes");
+  });
+
   it("Check a11y recipes page", () => {
-    cy.visit("http://localhost:3000/Recipes");
     cy.injectAxe();
     cy.checkA11y(null, null, terminalLog, true);
   });
 
-  it("Can view recipe in new tab");
+  it("Can view recipe in new tab", () => {
+    cy.window().then((win) => {
+      cy.stub(win, "open").as("open");
+    });
+    cy.get("[data-testid='Chicken Satay Stir Fry']").click();
+    cy.get("@open").should("have.been.calledOnce");
+  });
 });
