d9597712833d56a6dc7fa6095dc3523ff7548c7c
Robert Sayles <robert.sayles@and.digital>
2022-11-08 10:39:43 +0000

    [ROT-374]: Fixed current tests using alias variables

--- cypress/e2e/build-your-plate.cy.ts
+++ cypress/e2e/build-your-plate.cy.ts
@@ -56,32 +56,68 @@ describe("Build Your Plate", () => {
     cy.get("button [alt='Meat']").click();
 
     const meats = { Beef: 9, Lamb: 9, Chicken: 9, Pork: 8, Turkey: 9, "Cooked Meats": 8 };
-    // let selectedItems = 0;
-    // let absentItems = 0;
-    let expectedScore = 0;
+    const expectedScore = [0];
+    cy.wrap(expectedScore).as("expectedScore");
     Object.entries(meats).forEach(([key, value]) => {
       cy.get("body").then((body) => {
         if (body.find(`button [alt='${key}']`).length) {
           cy.get(`button [alt='${key}']`).click();
-          // selectedItems += 1;
-          expectedScore += value;
-          // } else {
-          //   absentItems += 1;
+          cy.get<number[]>("@expectedScore").then((score) => {
+            score.push(score.pop() + value);
+          });
         }
       });
     });
-    // expect(selectedItems).to.be.eq(5);
-    // expect(absentItems).to.be.eq(1);
 
     cy.get("[alt='Tick']").should("have.length", 5);
     cy.contains("button", "Score my plate").should("be.enabled");
     cy.contains("button", "Score my plate").click();
     cy.get("[alt='plate']");
     cy.contains("button", "Score my plate").click();
-    cy.contains(`Score: ${expectedScore} out of 50`);
+    cy.get<number[]>("@expectedScore").then((score) => {
+      cy.contains(`Score: ${score[0]} out of 50`);
+    });
     cy.contains("Great!");
     cy.contains("button", "Play again").click();
   });
+
   it("gives correct score with 5 mixed items");
-  it("allows user to swap food items before scoring");
+
+  it("allows user to swap food items before scoring (on mobile)", () => {
+    cy.viewport(768, 1024);
+
+    cy.get("[data-testid='game-begin']").click();
+    cy.contains("button", "Play").click();
+
+    cy.get("button [alt='Sweets']").click();
+    const sweets = ["Chocolate", "Jelly Sweets", "Cupcakes", "Biscuits", "Donuts", "Ice Cream"];
+    const selectedSweets = [];
+    cy.wrap(selectedSweets).as("selectedSweets");
+    sweets.forEach((item) => {
+      cy.get("body").then((body) => {
+        if (body.find(`button [alt='${item}']`).length) {
+          cy.get(`button [alt='${item}']`).click();
+          cy.get<string[]>("@selectedSweets").then((selected) => {
+            selected.push(item);
+          });
+        }
+      });
+    });
+
+    cy.log(`${selectedSweets}`);
+    cy.contains("button", "Score my plate").click();
+    cy.get("[alt='plate']");
+    selectedSweets.forEach((item) => cy.get(`button [alt='${item}']`));
+    cy.contains("button", "Swap Food").click();
+
+    cy.get("[alt='Tick']").should("have.length", 5);
+    cy.get<string[]>("@selectedSweets").then((selected) => {
+      // Need to force click as cypress thinks the tick covers the button
+      cy.get(`button [alt='${selected[0]}']`).filter(":visible").click({ force: true });
+    });
+    cy.get("[alt='Tick']").should("have.length", 4);
+    cy.get("button [alt='Fish']").click();
+    cy.get("button [alt='Tuna']").click();
+    cy.get("[alt='Tick']").should("have.length", 5);
+  });
 });
