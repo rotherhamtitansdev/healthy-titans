904ceebd2c5afac3ec4e26bb3470e0643372bf6c
RobertSaylesAND <79990531+RobertSaylesAND@users.noreply.github.com>
2022-11-10 14:44:42 +0000

    [ROT-389]: Applied alias wrapping to variable, extended test to cover suggestion button functionality (#129)

--- cypress/e2e/food-nutrition.cy.ts
+++ cypress/e2e/food-nutrition.cy.ts
@@ -25,17 +25,14 @@ describe("Food & Nutrition", () => {
   });
 
   it("Can view fried chicken information on mobile", () => {
-    cy.viewport(768, 1024)
+    cy.viewport(768, 1024);
     cy.findByRole("button", { name: /menu-toggle/i }).click();
     cy.get('[data-testid="mobile-menu"] [href="/FoodAndNutrition"]').click();
     cy.get('[data-testid="Fast Food"]').click();
     cy.get('[data-testid="Fried Chicken"]').click();
-    cy.url().should("eq", "http://localhost:3000/FoodAndNutrition/FastFood/FriedChicken")
+    cy.url().should("eq", "http://localhost:3000/FoodAndNutrition/FastFood/FriedChicken");
 
-    cy.get('[data-testid="details-title"]').should(
-      "have.text",
-      foodNutrition.friedChicken.title
-    );
+    cy.get('[data-testid="details-title"]').should("have.text", foodNutrition.friedChicken.title);
     cy.get('[data-testid="details-description"]').should(
       "have.text",
       foodNutrition.friedChicken.description
@@ -47,16 +44,13 @@ describe("Food & Nutrition", () => {
   });
 
   it("Can view fried chicken information on larger screens", () => {
-    cy.viewport(1440, 900)
+    cy.viewport(1440, 900);
     cy.get('[data-testid="desktop-navbar-links"] [href="/FoodAndNutrition"]').click();
     cy.get('[data-testid="Fast Food"]').click();
     cy.get('[data-testid="Fried Chicken"]').click();
-    cy.url().should("eq", "http://localhost:3000/FoodAndNutrition/FastFood/FriedChicken")
+    cy.url().should("eq", "http://localhost:3000/FoodAndNutrition/FastFood/FriedChicken");
 
-    cy.get('[data-testid="details-title"]').should(
-      "have.text",
-      foodNutrition.friedChicken.title
-    );
+    cy.get('[data-testid="details-title"]').should("have.text", foodNutrition.friedChicken.title);
     cy.get('[data-testid="details-description"]').should(
       "have.text",
       foodNutrition.friedChicken.description
@@ -70,32 +64,34 @@ describe("Food & Nutrition", () => {
   it("Can view carbohydrates information", () => {
     cy.get('.react-multi-carousel-list [data-testid="Food & Nutrition"]').click();
     cy.get('[data-testid="Carbs"]').click();
-    cy.url().should("eq", "http://localhost:3000/FoodAndNutrition/Nutrition/Carbs")
+    cy.url().should("eq", "http://localhost:3000/FoodAndNutrition/Nutrition/Carbs");
 
-    cy.get('[data-testid="details-title"]').should(
-      "have.text",
-      foodNutrition.carbs.title
-    );
+    cy.get('[data-testid="details-title"]').should("have.text", foodNutrition.carbs.title);
     cy.get('[data-testid="details-description"]').should(
       "have.text",
       foodNutrition.carbs.description
     );
   });
 
-  it("Can view 'next' suggestions", () => {
+  it("Can view and use 'next' suggestions", () => {
     cy.get('.react-multi-carousel-list [data-testid="Food & Nutrition"]').click();
     cy.get('[data-testid="Dairy & Eggs"]').click();
     cy.get('[data-testid="Cheese"]').click();
-    cy.url().should("eq", "http://localhost:3000/FoodAndNutrition/DairyAndEggs/Cheese")
+    cy.url().should("eq", "http://localhost:3000/FoodAndNutrition/DairyAndEggs/Cheese");
 
-    cy.get(".react-multi-carousel-list").should("be.visible")
-    cy.get(".react-multi-carousel-item").should("have.length", 3)
-    
-    const NonCheeseDairyAndEggsItems = ["Butter", "Eggs", "Milk", "Yoghurt", "Milkshakes"]
-    const nextItems = []
-    cy.get(".react-multi-carousel-item").each($item => {
-      nextItems.push($item.find("[class*='text-homepageHeaderText']").text())
-    })
-    expect(NonCheeseDairyAndEggsItems).to.include.members(nextItems)
-  })
+    cy.get(".react-multi-carousel-list").should("be.visible");
+    cy.get(".react-multi-carousel-item").should("have.length", 3);
+
+    const NonCheeseDairyAndEggsItems = ["Butter", "Eggs", "Milk", "Yoghurt", "Milkshakes"];
+    const nextItems = [];
+    cy.wrap(nextItems).as("nextItems");
+    cy.get(".react-multi-carousel-item").each((item) => {
+      nextItems.push(item.find("[class*='text-homepageHeaderText']").text());
+    });
+    cy.get<string[]>("@nextItems").then((items) => {
+      expect(NonCheeseDairyAndEggsItems).to.include.members(items);
+      cy.get(`.react-multi-carousel-item [data-testid=${items[0]}]`).click();
+      cy.url().should("eq", `http://localhost:3000/FoodAndNutrition/DairyAndEggs/${items[0]}`);
+    });
+  });
 });
