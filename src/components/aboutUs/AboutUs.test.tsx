827b54bc232d8b8696463869627d0345f48486e7
ALougherANDDigital <andrew.lougher@and.digital>
2022-11-14 15:42:58 +0000

    fix aboutus test

--- src/components/aboutUs/AboutUs.test.tsx
+++ src/components/aboutUs/AboutUs.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-// import * as FirebaseAPI from "../../api/FirebaseAPI";
+import * as FirebaseAPI from "../../api/FirebaseAPI";
 import AboutUs from "./AboutUs";
 
 const mockContent = {
@@ -14,19 +14,15 @@ const mockContent = {
   section_2: ["section 2 header", "section 2 body", "section 2 body 2"],
   section_3: ["section 3 header", "section 3 body"],
   section_4: ["section 4 header", "section 4 body"],
-  section_5: ["section 4 header", "section 4 body", "section 4 body 2"],
+  section_5: ["section 5 header", "section 5 body", "section 5 body 2"],
 };
 
 describe("About Us", () => {
   test("component renders with sections and image", async () => {
-    jest.mock("../../api/FirebaseAPI", () => ({
-      ...jest.requireActual("../../api/FirebaseAPI"),
-      fetchImages: jest.fn().mockReturnValue("fruit.jpg"),
-      fetchDataFromSubpath: jest.fn().mockReturnValue(mockContent),
-    }));
-
-    // jest.spyOn(FirebaseAPI, "fetchDataFromSubpath").mockResolvedValueOnce(mockContent);
-    // jest.spyOn(FirebaseAPI, "fetchImages").mockResolvedValue("fruit.jpg");
+    jest.spyOn(FirebaseAPI, "fetchDataFromSubpath").mockResolvedValue(mockContent);
+    jest
+      .spyOn(FirebaseAPI, "fetchAllImages")
+      .mockResolvedValue(["fruit.jpg", "fruit.jpg", "fruit.jpg"]);
 
     const { asFragment } = render(<AboutUs />);
 
