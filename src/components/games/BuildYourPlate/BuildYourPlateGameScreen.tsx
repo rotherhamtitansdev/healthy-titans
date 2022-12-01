5075d07fe846cea07056a7cf4bc82e027451a8f7
hannah-whelan <hannah.whelan@and.digital>
2022-12-01 10:12:50 +0000

    removed hover text for mobile/ipad

--- src/components/games/BuildYourPlate/BuildYourPlateGameScreen.tsx
+++ src/components/games/BuildYourPlate/BuildYourPlateGameScreen.tsx
@@ -204,7 +204,7 @@ const BuildYourPlateGameScreen = () => {
                             className="relative text-center group"
                           >
                             {cell.icon}
-                            <span className="text-[8px] bg-white bg-opacity-70 absolute rounded top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 invisible sm:text-xs group-hover:visible">
+                            <span className="text-[8px] bg-white bg-opacity-70 absolute rounded top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 invisible sm:text-xs lg:group-hover:visible">
                               {cell.name}
                             </span>
                           </button>
